68eef0d108df81d19c80c669e8fd699a1e51b1bd
a-sakhizada <arezousakhizada@gmail.com>
2022-08-12 02:50:21 -0400

    add employee fields ar ebeing read from the modal and is successfully redirecting to the home page after

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Employee = require('../models/employees'); //require the employee schema
+const multer = require('multer');  //processes files that are uploaded
+
+//file upload
+let storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, './uploads');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
+});
+
+let upload = multer({
+  storage: storage,
+}).single('file');
 
 //handling all routes manipulating employees
 router.get("/", (req, res) => {
@@ -10,6 +26,29 @@ router.get("/", (req, res) => {
 router.get("/add", (req, res) => {
     const templateVars = {title: 'Add Employees'};
   res.render('add_employees', templateVars);
+});
+
+//add an employee to the db
+router.post("/add", upload, (req, res) => {
+  const employee = new Employee({
+    name: req.body.name,
+    email: req.body.email,
+    department: req.body.department,
+    employeeStatus: req.body.employeeStatus,
+    Files: req.file.filename,
+  });
+  employee.save((err) => {
+    if(err){
+      res.json({message: err.message, type: 'danger'});
+    }else{
+      req.session.message = {
+        type: 'success',
+        message: 'User added successfully!'
+      };
+      res.redirect('/');
+    }
+  });
+
 });
 
 
