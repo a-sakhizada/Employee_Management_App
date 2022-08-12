88aa1a4e5635e24546be89d6609fd3abd053e969
a-sakhizada <arezousakhizada@gmail.com>
2022-08-11 23:42:02 -0400

    connect to the mongoDB using monogoose and the db_uri

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//database connection
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to the database'));
+
 app.get('/', (req, res) => {
     res.send('hi');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
