aea24ed366f1289cfc88366fe7e31102c5a7b003
a-sakhizada <arezousakhizada@gmail.com>
2022-08-12 13:54:02 -0400

    added edit employee modal for updating employee details

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const Employee = require('../models/employees'); //require the employee schema
-const multer = require("multer");  //processes files that are uploaded
+const Employee = require("../models/employees"); //require the employee schema
+const multer = require("multer"); //processes files that are uploaded
+const fs = require("fs");
 
 //file upload
 let storage = multer.diskStorage({
-  destination: function(req, file, cb){
+  destination: function (req, file, cb) {
     cb(null, "./uploads");
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
   },
-//   filename:  function (req, file, cb) { 
-//     cb(null , file.originalname);   
-//  },
+  //   filename:  function (req, file, cb) {
+  //     cb(null , file.originalname);
+  //  },
 });
 
 let upload = multer({
@@ -29,8 +30,8 @@ let upload = multer({
 // });
 
 router.get("/add", (req, res) => {
-    const templateVars = {title: 'Add Employees'};
-  res.render('add_employees', templateVars);
+  const templateVars = { title: "Add Employees" };
+  res.render("add_employees", templateVars);
 });
 
 //add an employee to the db
@@ -43,33 +44,86 @@ router.post("/add", upload, (req, res) => {
     file: req.file.filename,
   });
   employee.save((err) => {
+    if (err) {
+      res.json({ message: err.message, type: "danger" });
+    } else {
+      req.session.message = {
+        type: "success",
+        message: "Employee added successfully!",
+      };
+      res.redirect("/");
+    }
+  });
+});
+
+// get all employees
+router.get("/", (req, res) => {
+  Employee.find().exec((err, employees) => {
+    if (err) {
+      res.json({ message: err.message });
+    } else {
+      console.log("employees obj:", employees);
+      res.render("index", {
+        title: "Home Page",
+        employees: employees,
+      });
+    }
+  });
+});
+
+//edit employee page
+router.get("/edit/:id", (req, res) => {
+  let id = req.params.id;
+  Employee.findById(id, (err, employee) => {
+    if (err) {
+      res.redirect("/");
+    } else {
+      if (employee == null) {
+        res.redirect("/");
+      } else {
+        res.render("edit_employees", {
+          title: "Edit Employees",
+          employee: employee,
+        });
+      }
+    }
+  });
+});
+
+//update employee
+router.post("/update/:id", upload, (req, res) => {
+  let id = req.params.id;
+  let new_file = "";
+
+  if (req.file) {
+    new_file = req.file.filename;
+    try {
+      fs.unlinkSync("./uploads" + req.body.old_file);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    new_file = req.body.old_file;
+  }
+
+  //get all other employee details
+  Employee.findByIdAndUpdate(id, {
+    name: req.body.name,
+    email: req.body.email,
+    department: req.body.department,
+    employeeStatus: req.body.employeeStatus,
+    file: new_file,
+  }, (err) => {
     if(err){
       res.json({message: err.message, type: 'danger'});
     }else{
       req.session.message = {
         type: 'success',
-        message: 'Employee added successfully!'
+        message: 'Employee updated successfully!',
       };
       res.redirect('/');
     }
   });
-
 });
-
-// get all employees
-router.get('/', (req, res) => {
-  Employee.find().exec((err, employees) => {
-    if(err){
-      res.json({ message: err.message });
-    }else {
-      console.log("employees obj:", employees);
-      res.render('index', {
-        title: "Home Page",
-        employees: employees,
-      })
-    }
-  });
-});
-
 
 module.exports = router;
